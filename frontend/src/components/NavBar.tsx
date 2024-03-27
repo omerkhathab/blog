a942414f84f44f8ece491e591e714cc804fa841f
Omer Khathab <okhathab@gmail.com>
2024-03-28 00:10:12 +0530

    added logout and some auth

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,6 +1,11 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 export const NavBar = ({type = ""}) => {
+    const navigate = useNavigate();
+    const removeAuth = () => {
+        localStorage.removeItem("authorization");
+        navigate('/signin')
+    }
     return (
         <div className="flex w-full bg-slate-100 justify-between items-center px-10 py-5">
             <Link to={"/blogs"}>
@@ -11,6 +16,7 @@ export const NavBar = ({type = ""}) => {
                     {/* <div className="pl-20 text-2xl">{ type == 'publish' ? "Publish" : "New Blog" }</div> */}
                     <button type="button" className="text-white bg-green-600 hover:bg-green-700 focus:outline-none font-medium rounded-full text-md px-5 py-2.5 text-center me-2">{ type == 'publish' ? "Publish" : "New" }</button>
                 </Link>
+                <button onClick={removeAuth} type="button" className="text-white bg-red-600 hover:bg-red-700 focus:outline-none font-medium rounded-full text-md px-5 py-2.5 text-center me-2">Logout</button>
             </div>   
         </div>
     )
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,9 +1,12 @@
+import { Navigate } from "react-router-dom";
 import { BlogCard } from "../components/BlogCard"
 import { Loader } from "../components/Loader";
 import { NavBar } from "../components/NavBar"
 import { useBlogs } from "../hooks"
 export const Blogs = () => {
-
+    if(!localStorage.getItem("authorization")){
+        return <Navigate to={'/signin'} />
+    }
     const {loading, blogs} = useBlogs();
     
     console.log(loading, blogs)
